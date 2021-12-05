123778bcc4d48d7b65b159ff055623c7f117cb55
yasinidlbi <82759297+yasinidlbi@users.noreply.github.com>
2021-12-05 17:49:49 +0200

    s

--- Restaurant/Restaurant/View/sales/uc_diningHall.xaml.cs
+++ Restaurant/Restaurant/View/sales/uc_diningHall.xaml.cs
@@ -1,6 +1,8 @@
-﻿using Restaurant.Classes;
+﻿using MaterialDesignThemes.Wpf;
+using Restaurant.Classes;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +14,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
+using System.Windows.Resources;
 using System.Windows.Shapes;
 
 namespace Restaurant.View.sales
@@ -49,8 +52,8 @@ namespace Restaurant.View.sales
             // for pagination onTop Always
             btns = new Button[] { btn_firstPage, btn_prevPage, btn_activePage, btn_nextPage, btn_lastPage };
             catigoriesAndItemsView.ucdiningHall = this;
-
-           await Search();
+            FillBillDetailsList(0);
+            await Search();
         }
         private void UserControl_Unloaded(object sender, RoutedEventArgs e)
         {
@@ -282,6 +285,272 @@ namespace Restaurant.View.sales
             }
         }
         #endregion
+        #endregion
+
+        #region invoice
+        ObservableCollection<BillDetails> billDetailsList = new ObservableCollection<BillDetails>();
+
+        public class BillDetails
+        {
+            public int index { get; set; }
+            public string image { get; set; }
+            public int itemId { get; set; }
+            public string itemName { get; set; }
+            public int Count { get; set; }
+            public decimal Price { get; set; }
+            public decimal Total { get; set; }
+            public decimal Tax { get; set; }
+            public int? offerId { get; set; }
+           
+        }
+        private async Task FillBillDetailsList(int invoiceId)
+        {
+            ////get invoice items
+            //invoiceItems = await invoiceModel.GetInvoicesItems(invoiceId);
+            //// build invoice details grid
+            billDetailsList.Clear();
+            //test
+            Random r = new Random();
+            //int rInt = r.Next(0, 100);
+                for (int i = 0; i < 13; i++)
+                {
+
+                 int   _Count = r.Next(0, 25);
+                Decimal _Price = (r.Next(0, 10000)) / 100;
+                billDetailsList.Add(new BillDetails()
+                {
+                    index = i,
+                    image = "/pic/90408.jpg",
+                    itemId = (i + 1),
+                    itemName = "name - " + (i + 1),
+                    Count = _Count,
+                    Price = _Price,
+                    Total = _Count * _Price,
+                    offerId =0,
+                });
+            }
+            //
+            //int indexer = 0;
+            //foreach (ItemTransfer itemT in invoiceItems)
+            //{
+            //    decimal total = (decimal)(itemT.price * itemT.quantity);
+            //    billDetailsList.Add(new BillDetails()
+            //    {
+            //        ID = indexer,
+            //        Product = itemT.itemName,
+            //        itemId = (int)itemT.itemId,
+            //        Unit = itemT.itemUnitId.ToString(),
+            //        itemUnitId = (int)itemT.itemUnitId,
+            //        Count = (int)itemT.quantity,
+            //        Price = (decimal)itemT.price,
+            //        Total = total,
+            //        serialList = serialNumLst,
+            //        type = itemT.itemType,
+            //        valid = isValid,
+            //        offerId = itemT.offerId,
+            //    });
+            //    indexer++;
+            //}
+            BuildBillDesign();
+        }
+        void BuildBillDesign()
+        {
+            //sv_billDetail.Children.Clear();
+            #region Grid Container
+            Grid gridContainer = new Grid();
+            gridContainer.Margin = new Thickness(5);
+            //int rowCount = billDetailsList.Count();
+            int rowCount = 13;
+            RowDefinition[] rd = new RowDefinition[rowCount];
+            for (int i = 0; i < rowCount; i++)
+            {
+                rd[i] = new RowDefinition();
+                rd[i].Height = new GridLength(1, GridUnitType.Auto);
+            }
+            for (int i = 0; i < rowCount; i++)
+            {
+                gridContainer.RowDefinitions.Add(rd[i]);
+            }
+            /////////////////////////////////////////////////////
+            int colCount = 7;
+            ColumnDefinition[] cd = new ColumnDefinition[colCount];
+            for (int i = 0; i < colCount; i++)
+            {
+                cd[i] = new ColumnDefinition();
+            }
+            cd[0].Width = new GridLength(1, GridUnitType.Auto);
+            cd[1].Width = new GridLength(1, GridUnitType.Auto);
+            cd[2].Width = new GridLength(1, GridUnitType.Star);
+            cd[3].Width = new GridLength(1, GridUnitType.Auto);
+            cd[4].Width = new GridLength(1, GridUnitType.Auto);
+            cd[5].Width = new GridLength(1, GridUnitType.Auto);
+            cd[6].Width = new GridLength(1, GridUnitType.Star);
+            for (int i = 0; i < colCount; i++)
+            {
+                gridContainer.ColumnDefinitions.Add(cd[i]);
+            }
+            /////////////////////////////////////////////////////
+
+
+
+            #endregion
+            foreach (var item in billDetailsList)
+            {
+                #region   index
+                var indexText = new TextBlock();
+                indexText.Text = (item.index + 1)+".";
+                indexText.Tag = item.index;
+                indexText.Foreground = Application.Current.Resources["SecondColor"] as SolidColorBrush;
+                indexText.FontSize = 14;
+                indexText.FontWeight = FontWeights.Bold;
+                indexText.Margin = new Thickness(5);
+                indexText.VerticalAlignment = VerticalAlignment.Center;
+                indexText.HorizontalAlignment = HorizontalAlignment.Center;
+
+                Grid.SetRow(indexText, item.index);
+                Grid.SetColumn(indexText, 0);
+                gridContainer.Children.Add(indexText);
+                /////////////////////////////////
+
+                #endregion
+                #region   image
+                Button buttonImage = new Button();
+                buttonImage.Tag = item.index;
+                buttonImage.Background = (SolidColorBrush)(new BrushConverter().ConvertFrom("#FFFFFF"));
+                buttonImage.Height =  
+                buttonImage.Width = 40;
+                buttonImage.Margin = new Thickness(5 , 10, 5, 10);
+                buttonImage.VerticalAlignment = VerticalAlignment.Center;
+                buttonImage.HorizontalAlignment = HorizontalAlignment.Center;
+                buttonImage.BorderThickness = new Thickness(1);
+                buttonImage.BorderBrush = Application.Current.Resources["Grey"] as SolidColorBrush;
+                buttonImage.Padding = new Thickness(0);
+                buttonImage.FlowDirection = FlowDirection.LeftToRight;
+                MaterialDesignThemes.Wpf.ButtonAssist.SetCornerRadius(buttonImage, (new CornerRadius(10)));
+                //bool isModified = HelpClass.chkImgChng(cardViewitem.item.image, (DateTime)cardViewitem.item.updateDate, Global.TMPItemsFolder);
+                //if (isModified)
+                //    HelpClass.getImg("Item", cardViewitem.item.image, buttonImage);
+                //else
+                //    HelpClass.getLocalImg("Item", cardViewitem.item.image, buttonImage);
+                fillEllipse(buttonImage);
+
+                Grid.SetRow(buttonImage, item.index);
+                Grid.SetColumn(buttonImage, 1);
+                gridContainer.Children.Add(buttonImage);
+                /////////////////////////////////
+
+                #endregion
+                #region   name
+                var itemNameText = new TextBlock();
+                itemNameText.Text = item.itemName;
+                itemNameText.Tag = item.index;
+                itemNameText.Foreground = Application.Current.Resources["SecondColor"] as SolidColorBrush;
+                itemNameText.FontSize = 14;
+                itemNameText.Margin = new Thickness(5);
+                itemNameText.VerticalAlignment = VerticalAlignment.Center;
+                itemNameText.HorizontalAlignment = HorizontalAlignment.Left;
+                itemNameText.FontWeight = FontWeights.Bold;
+
+                Grid.SetRow(itemNameText, item.index);
+                Grid.SetColumn(itemNameText, 2);
+                gridContainer.Children.Add(itemNameText);
+                #endregion
+                #region   Count
+                var countText = new TextBlock();
+                countText.Text = item.Count.ToString();
+                countText.Tag = item.index;
+                countText.Foreground = Application.Current.Resources["SecondColor"] as SolidColorBrush;
+                countText.FontSize = 14;
+                countText.FontWeight = FontWeights.Bold;
+                countText.Margin = new Thickness(5);
+                countText.VerticalAlignment = VerticalAlignment.Center;
+                countText.HorizontalAlignment = HorizontalAlignment.Center;
+
+                Grid.SetRow(countText, item.index);
+                Grid.SetColumn(countText, 4);
+                gridContainer.Children.Add(countText);
+                /////////////////////////////////
+
+                #endregion
+                #region   Total
+                var totalText = new TextBlock();
+                totalText.Text = item.Total.ToString();
+                totalText.Tag = item.index;
+                totalText.Foreground = Application.Current.Resources["Grey"] as SolidColorBrush;
+                totalText.Margin = new Thickness(5);
+                totalText.VerticalAlignment = VerticalAlignment.Center;
+                totalText.HorizontalAlignment = HorizontalAlignment.Right;
+
+                Grid.SetRow(totalText, item.index);
+                Grid.SetColumn(totalText, 6);
+                gridContainer.Children.Add(totalText);
+                /////////////////////////////////
+
+                #endregion
+
+                #region   Minus
+                //                         Width = "25" Height = "25" Kind = "Minus" />
+
+                Button buttonMinus = new Button();
+                buttonMinus.Tag = item.index;
+                buttonMinus.Margin = new Thickness(2.5);
+                buttonMinus.BorderThickness = new Thickness(0);
+                buttonMinus.Height =
+                buttonMinus.Width = 30;
+                buttonMinus.Padding = new Thickness(0);
+                buttonMinus.Background = (SolidColorBrush)(new BrushConverter().ConvertFrom("#DFDFDF"));
+
+                #region materialDesign
+                var MinusPackIcon = new PackIcon();
+                MinusPackIcon.Foreground = Application.Current.Resources["SecondColor"] as SolidColorBrush;
+                MinusPackIcon.Height =
+                MinusPackIcon.Width = 30;
+                MinusPackIcon.Kind = PackIconKind.Minus;
+                #endregion
+                buttonMinus.Content = MinusPackIcon;
+
+                Grid.SetRow(buttonMinus, item.index);
+                Grid.SetColumn(buttonMinus, 3);
+                gridContainer.Children.Add(buttonMinus);
+                /////////////////////////////////
+
+                #endregion
+                /*
+
+            <!--#region  1-->
+                                
+                                
+                                
+                                <Button Tag="ssssssss" Grid.Column="5"    Margin="2.5"
+                                    Height="25" Width="25"  Padding="0"
+                                        Background="#DFDFDF"   BorderThickness="0">
+                                    <materialDesign:PackIcon   Foreground="{StaticResource SecondColor}"
+                                         Width="25" Height="25" Kind="Plus" />
+                                </Button>
+                               
+                                <!--#endregion-->
+                  
+                 * */
+            }
+            sv_billDetail.Content = gridContainer;
+
+        }
+
+        void fillEllipse(Button img)
+        {
+            try
+            {
+                ImageBrush myBrush = new ImageBrush();
+                Uri resourceUri = new Uri("/pic/90408.jpg", UriKind.Relative);
+                StreamResourceInfo streamInfo = Application.GetResourceStream(resourceUri);
+                BitmapFrame temp = BitmapFrame.Create(streamInfo.Stream);
+                myBrush.ImageSource = temp;
+                img.Background = myBrush;
+            }
+            catch
+            { }
+        }
+       
         #endregion
     }
 }
--- Restaurant/Restaurant/controlTemplate/uc_squareCard.xaml.cs
+++ Restaurant/Restaurant/controlTemplate/uc_squareCard.xaml.cs
@@ -130,109 +130,64 @@ namespace Restaurant.controlTemplate
             #endregion
 
             if (cardViewitem.item.isNew == 1)
+            //if (true)
             {
                
                 #region Path newLabel
                 Path pathNewLabel = new Path();
-                Grid.SetRowSpan(pathNewLabel, 4);
                 pathNewLabel.Fill = (SolidColorBrush)(new BrushConverter().ConvertFrom("#D20707"));
                 pathNewLabel.Stretch = Stretch.Fill;
-                Grid.SetColumnSpan(pathNewLabel, 2);
-                //pathStar.Height = 18;
-                //pathStar.Width = 54;
-                //pathNewLabel.VerticalAlignment = VerticalAlignment.Bottom;
-                //pathNewLabel.HorizontalAlignment = HorizontalAlignment.Right;
                 pathNewLabel.FlowDirection = FlowDirection.LeftToRight;
-                //pathNewLabel.Margin = new Thickness(7.5);
-                pathNewLabel.Data = App.Current.Resources["newBlock"] as Geometry;
-                pathNewLabel.Width = gridContainer.Width / 6.5;
+                pathNewLabel.Data = App.Current.Resources["rectangleBlock"] as Geometry;
+                pathNewLabel.Width = brd_main.ActualHeight / 2.5;
                 pathNewLabel.Height = pathNewLabel.Width / 3;
                 #region Text
                 Path pathNewLabelText = new Path();
-                Grid.SetRowSpan(pathNewLabelText, 4);
                 pathNewLabelText.Fill = (SolidColorBrush)(new BrushConverter().ConvertFrom("#FFFD00"));
                 pathNewLabelText.Stretch = Stretch.Fill;
-                Grid.SetColumnSpan(pathNewLabelText, 2);
-
-                //pathNewLabelText.VerticalAlignment = VerticalAlignment.Bottom;
-                //pathNewLabelText.HorizontalAlignment = HorizontalAlignment.Right;
                 pathNewLabelText.FlowDirection = FlowDirection.LeftToRight;
-                //pathNewLabelText.Margin = new Thickness(7.5);
-                //pathNewLabelText.Margin = new Thickness(0, 0, 12.5, 10);
                 pathNewLabelText.Data = App.Current.Resources["newText"] as Geometry;
-                //pathStar.Height = 18;
-                //pathStar.Width = 54;
-                pathNewLabelText.Width = gridContainer.Width / 10;
+                pathNewLabelText.Width = brd_main.ActualHeight / 4;
                 pathNewLabelText.Height = pathNewLabelText.Width / 3;
                 #endregion
                 #endregion
-
                 Grid gridNewContainer = new Grid();
-                Grid.SetRowSpan(gridNewContainer, 4);
-                Grid.SetColumnSpan(gridNewContainer, 2);
                 gridNewContainer.VerticalAlignment = VerticalAlignment.Bottom;
                 gridNewContainer.HorizontalAlignment = HorizontalAlignment.Right;
-                gridNewContainer.Margin = new Thickness(7.5);
-
+                gridNewContainer.Margin = new Thickness(0, 7.5, 0, 7.5);
                 gridNewContainer.Children.Add(pathNewLabel);
                 gridNewContainer.Children.Add(pathNewLabelText);
-
                 gridContainer.Children.Add(gridNewContainer);
 
-                //gridContainer.Children.Add(pathNewLabel);
-                //gridContainer.Children.Add(pathNewLabelText);
             }
             if (cardViewitem.item.isOffer == 1)
+                //if (true)
             {
                 #region Path offerLabel
-                //string dataStar = "";
                 Path pathOfferLabel = new Path();
-                Grid.SetColumnSpan(pathOfferLabel, 2);
-                Grid.SetRowSpan(pathOfferLabel, 4);
                 pathOfferLabel.Fill = (SolidColorBrush)(new BrushConverter().ConvertFrom("#D20707"));
-                pathOfferLabel.VerticalAlignment = VerticalAlignment.Top;
                 pathOfferLabel.Stretch = Stretch.Fill;
-                //   Height = "16" Width = "86" 
-                pathOfferLabel.Height = pathOfferLabel.Width = gridContainer.Width / 4.5;
                 pathOfferLabel.FlowDirection = FlowDirection.LeftToRight;
-                pathOfferLabel.HorizontalAlignment = HorizontalAlignment.Right;
-
-                if (MainWindow.lang.Equals("ar"))
-                {
-                    pathOfferLabel.Data = App.Current.Resources["offerLabelArTopLeft"] as Geometry;
-                }
-                else
-                {
-                    pathOfferLabel.Data = App.Current.Resources["offerLabelEnTopRight"] as Geometry;
-                }
-
+                pathOfferLabel.Data = App.Current.Resources["rectangleBlock"] as Geometry;
+                pathOfferLabel.Width = brd_main.ActualHeight / 2.1;
+                pathOfferLabel.Height = pathOfferLabel.Width / 3;
                 #region Text
                 Path pathOfferLabelText = new Path();
-                Grid.SetColumnSpan(pathOfferLabelText, 2);
-                Grid.SetRowSpan(pathOfferLabelText, 4);
-                pathOfferLabelText.FlowDirection = FlowDirection.LeftToRight;
-                pathOfferLabelText.VerticalAlignment = VerticalAlignment.Top;
                 pathOfferLabelText.Fill = (SolidColorBrush)(new BrushConverter().ConvertFrom("#FFFD00"));
                 pathOfferLabelText.Stretch = Stretch.Fill;
-                pathOfferLabelText.HorizontalAlignment = HorizontalAlignment.Right;
-                if (MainWindow.lang.Equals("ar"))
-                {
-                    pathOfferLabelText.Height = pathOfferLabelText.Width = gridContainer.Width / 7;
-                    pathOfferLabelText.Margin = new Thickness(0, 4, 4, 0);
-                    pathOfferLabelText.Data = App.Current.Resources["offerLabelArTopLeft_Text"] as Geometry;
-                }
-                else
-
-                {
-                    pathOfferLabelText.Height = pathOfferLabelText.Width = gridContainer.Width / 6.5;
-                    pathOfferLabelText.Margin = new Thickness(0, 2.5, 2.5, 0);
-                    pathOfferLabelText.Data = App.Current.Resources["offerLabelEnTopRight_Text"] as Geometry;
-                }
-
+                pathOfferLabelText.FlowDirection = FlowDirection.LeftToRight;
+                pathOfferLabelText.Data = App.Current.Resources["offerText"] as Geometry;
+                pathOfferLabelText.Width = brd_main.ActualHeight /3;
+                pathOfferLabelText.Height = pathOfferLabelText.Width / 3;
                 #endregion
                 #endregion
-                gridContainer.Children.Add(pathOfferLabel);
-                gridContainer.Children.Add(pathOfferLabelText);
+                Grid gridOfferContainer = new Grid();
+                gridOfferContainer.VerticalAlignment = VerticalAlignment.Top;
+                gridOfferContainer.HorizontalAlignment = HorizontalAlignment.Left;
+                gridOfferContainer.Margin = new Thickness(0, 7.5, 0, 7.5);
+                gridOfferContainer.Children.Add(pathOfferLabel);
+                gridOfferContainer.Children.Add(pathOfferLabelText);
+                gridContainer.Children.Add(gridOfferContainer);
             }
             //if (cardViewitem.item.itemCount > 0)
             //{
