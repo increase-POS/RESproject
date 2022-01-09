38fa7322f4aa123186ad324716fb9234ba2089e0
yasinidlbi <82759297+yasinidlbi@users.noreply.github.com>
2022-01-09 16:00:21 +0200

    Update translateCategoryNameConverter.cs

--- Restaurant/Restaurant/converters/translateCategoryNameConverter.cs
+++ Restaurant/Restaurant/converters/translateCategoryNameConverter.cs
@@ -13,29 +13,34 @@ namespace Restaurant.converters
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-                Category category = value as Category;
-                string s = category.name ;
-
-            switch (s)
+            if (value != null && value != "")
             {
-                case "RawMaterials":
-                    s = MainWindow.resourcemanager.GetString("trRawMaterials");
-                    break;
-                case "Vegetables":
-                    s = MainWindow.resourcemanager.GetString("trVegetables");
-                    break;
-                case "Meat":
-                    s = MainWindow.resourcemanager.GetString("trMeat");
-                    break;
-                case "Drinks":
-                    s = MainWindow.resourcemanager.GetString("trDrinks");
-                    break;
-                default:
-                    s = s + "HelloWorld!!";
-                    break;
-            }
 
-            return s;
+                Category category = value as Category;
+                string s = category.name;
+
+                switch (s)
+                {
+                    case "RawMaterials":
+                        s = MainWindow.resourcemanager.GetString("trRawMaterials");
+                        break;
+                    case "Vegetables":
+                        s = MainWindow.resourcemanager.GetString("trVegetables");
+                        break;
+                    case "Meat":
+                        s = MainWindow.resourcemanager.GetString("trMeat");
+                        break;
+                    case "Drinks":
+                        s = MainWindow.resourcemanager.GetString("trDrinks");
+                        break;
+                    default:
+                        s = s + "HelloWorld!!";
+                        break;
+                }
+
+                return s;
+            }
+            else return value;
         }
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
