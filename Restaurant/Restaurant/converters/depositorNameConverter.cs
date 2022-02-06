d95b875629551372d317303fcef91695aae5d676
yasinidlbi <82759297+yasinidlbi@users.noreply.github.com>
2022-02-06 17:50:02 +0200

    converters

---
+++ Restaurant/Restaurant/converters/depositorNameConverter.cs
@@ -0,0 +1,56 @@
+﻿using Restaurant.Classes;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Restaurant.converters
+{
+    class depositorNameConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            CashTransfer s = value as CashTransfer;
+            string name = "";
+            switch (s.side)
+            {
+                case "bnd":break;
+                case "v": name = MainWindow.resourcemanager.GetString("trVendor"); break;
+                case "c": name = MainWindow.resourcemanager.GetString("trCustomer"); break;
+                case "u": name = MainWindow.resourcemanager.GetString("trUser"); break;
+                case "s": name = MainWindow.resourcemanager.GetString("trSalary"); break;
+                case "e": name = MainWindow.resourcemanager.GetString("trGeneralExpenses"); break;
+                case "m":
+                    if(s.transType=="d")
+                        name = MainWindow.resourcemanager.GetString("trAdministrativeDeposit");
+                    if(s.transType == "p")
+                        name = MainWindow.resourcemanager.GetString("trAdministrativePull");
+                    break;
+                case "sh": name = MainWindow.resourcemanager.GetString("trShippingCompany"); break;
+                default: break;
+            }
+
+            if (!string.IsNullOrEmpty(s.agentName))
+                name = name + " " + s.agentName;
+            else if (!string.IsNullOrEmpty(s.usersName) && !string.IsNullOrEmpty(s.usersLName))
+                name = name + " " + s.usersName + " " + s.usersLName;
+            else if (!string.IsNullOrEmpty(s.shippingCompanyName))
+                name = name + " " + s.shippingCompanyName;
+            else if ((s.side != "e") && (s.side != "m"))
+                name = name + " " + MainWindow.resourcemanager.GetString("trUnKnown");
+
+            return name;
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    
+
+}
---
+++ Restaurant/Restaurant/converters/processTypeConverter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Restaurant.converters
+{
+    class processTypeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string s = value as string;
+            switch (value)
+            {
+                case "cash": return MainWindow.resourcemanager.GetString("trCash");
+                    //break;
+                case "doc": return MainWindow.resourcemanager.GetString("trDocument");
+                    //break;
+                case "cheque": return MainWindow.resourcemanager.GetString("trCheque");
+                    //break;
+                case "balance": return MainWindow.resourcemanager.GetString("trCredit");
+                //break;
+                case "card": return MainWindow.resourcemanager.GetString("trAnotherPaymentMethods");
+                //break;
+                case "inv": return MainWindow.resourcemanager.GetString("trInv");
+                //break;
+                default: return s;
+                    //break;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
