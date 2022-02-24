410e7d3b27d07282127b7e7ba3add72f4aa3345f
yasinidlbi <82759297+yasinidlbi@users.noreply.github.com>
2022-02-24 17:50:09 +0200

    reports

--- Restaurant/Restaurant/View/reports/accountsReports/uc_accountsReports.xaml.cs
+++ Restaurant/Restaurant/View/reports/accountsReports/uc_accountsReports.xaml.cs
@@ -53,7 +53,7 @@ namespace Restaurant.View.reports.accountsReports
             try
             {
                 #region translate
-                if (MainWindow.lang.Equals("en"))
+                if (AppSettings.lang.Equals("en"))
                     grid_main.FlowDirection = FlowDirection.LeftToRight;
                 else
                     grid_main.FlowDirection = FlowDirection.RightToLeft;
@@ -71,7 +71,7 @@ namespace Restaurant.View.reports.accountsReports
                 await FillCombo.RefreshObjects();
             // Title
             if (!string.IsNullOrWhiteSpace(FillCombo.objectsList.Where(x => x.name == this.Tag.ToString()).FirstOrDefault().translate))
-                txt_mainTitle.Text = MainWindow.resourcemanager.GetString(
+                txt_mainTitle.Text = AppSettings.resourcemanager.GetString(
                FillCombo.objectsList.Where(x => x.name == this.Tag.ToString()).FirstOrDefault().translate
                );
             // Info
@@ -80,7 +80,7 @@ namespace Restaurant.View.reports.accountsReports
             foreach (var item in InfoTextBlocksList)
             {
                 if (!string.IsNullOrWhiteSpace(FillCombo.objectsList.Where(x => x.name == item.Tag.ToString()).FirstOrDefault().translate))
-                    item.Text = MainWindow.resourcemanager.GetString(
+                    item.Text = AppSettings.resourcemanager.GetString(
                    FillCombo.objectsList.Where(x => x.name == item.Tag.ToString()).FirstOrDefault().translate
                    );
             }
@@ -90,7 +90,7 @@ namespace Restaurant.View.reports.accountsReports
             foreach (var item in HintTextBlocksList)
             {
                 if (!string.IsNullOrWhiteSpace(FillCombo.objectsList.Where(x => x.name == item.Tag.ToString()).FirstOrDefault().translateHint))
-                    item.Text = MainWindow.resourcemanager.GetString(
+                    item.Text = AppSettings.resourcemanager.GetString(
                    FillCombo.objectsList.Where(x => x.name == item.Tag.ToString()).FirstOrDefault().translateHint
                    );
             }
@@ -99,82 +99,82 @@ namespace Restaurant.View.reports.accountsReports
 
         private void Btn_paymentsAccountsReports_Click(object sender, RoutedEventArgs e)
         {
-            //try
-            //{
-            //    grid_main.Children.Clear();
-            //    grid_main.Children.Add(uc_storageReports.Instance);
+            try
+            {
+                grid_main.Children.Clear();
+                grid_main.Children.Add(uc_paymentsAccountsReports.Instance);
 
-            //    Button button = sender as Button;
-            //    MainWindow.mainWindow.initializationMainTrack(button.Tag.ToString());
-            //}
-            //catch (Exception ex)
-            //{
-            //    HelpClass.ExceptionMessage(ex, this);
-            //}
+                Button button = sender as Button;
+                MainWindow.mainWindow.initializationMainTrack(button.Tag.ToString());
+            }
+            catch (Exception ex)
+            {
+                HelpClass.ExceptionMessage(ex, this);
+            }
         }
 
         private void Btn_recipientAccountsReports_Click(object sender, RoutedEventArgs e)
         {
-            //try
-            //{
-            //    grid_main.Children.Clear();
-            //    grid_main.Children.Add(uc_storageReports.Instance);
+            try
+            {
+                grid_main.Children.Clear();
+                grid_main.Children.Add(uc_recipientAccountsReports.Instance);
 
-            //    Button button = sender as Button;
-            //    MainWindow.mainWindow.initializationMainTrack(button.Tag.ToString());
-            //}
-            //catch (Exception ex)
-            //{
-            //    HelpClass.ExceptionMessage(ex, this);
-            //}
+                Button button = sender as Button;
+                MainWindow.mainWindow.initializationMainTrack(button.Tag.ToString());
+            }
+            catch (Exception ex)
+            {
+                HelpClass.ExceptionMessage(ex, this);
+            }
         }
 
         private void Btn_bankAccountsReports_Click(object sender, RoutedEventArgs e)
         {
-            //try
-            //{
-            //    grid_main.Children.Clear();
-            //    grid_main.Children.Add(uc_storageReports.Instance);
+            try
+            {
+                grid_main.Children.Clear();
+                grid_main.Children.Add(uc_bankAccountsReports.Instance);
 
-            //    Button button = sender as Button;
-            //    MainWindow.mainWindow.initializationMainTrack(button.Tag.ToString());
-            //}
-            //catch (Exception ex)
-            //{
-            //    HelpClass.ExceptionMessage(ex, this);
-            //}
+                Button button = sender as Button;
+                MainWindow.mainWindow.initializationMainTrack(button.Tag.ToString());
+            }
+            catch (Exception ex)
+            {
+                HelpClass.ExceptionMessage(ex, this);
+            }
         }
 
         private void Btn_posAccountsReports_Click(object sender, RoutedEventArgs e)
         {
-            //try
-            //{
-            //    grid_main.Children.Clear();
-            //    grid_main.Children.Add(uc_storageReports.Instance);
+            try
+            {
+                grid_main.Children.Clear();
+                grid_main.Children.Add(uc_posAccountsReports.Instance);
 
-            //    Button button = sender as Button;
-            //    MainWindow.mainWindow.initializationMainTrack(button.Tag.ToString());
-            //}
-            //catch (Exception ex)
-            //{
-            //    HelpClass.ExceptionMessage(ex, this);
-            //}
+                Button button = sender as Button;
+                MainWindow.mainWindow.initializationMainTrack(button.Tag.ToString());
+            }
+            catch (Exception ex)
+            {
+                HelpClass.ExceptionMessage(ex, this);
+            }
         }
 
         private void Btn_statementAccountsReports_Click(object sender, RoutedEventArgs e)
         {
-            //try
-            //{
-            //    grid_main.Children.Clear();
-            //    grid_main.Children.Add(uc_storageReports.Instance);
+            try
+            {
+                grid_main.Children.Clear();
+                grid_main.Children.Add(uc_statementAccountsReports.Instance);
 
-            //    Button button = sender as Button;
-            //    MainWindow.mainWindow.initializationMainTrack(button.Tag.ToString());
-            //}
-            //catch (Exception ex)
-            //{
-            //    HelpClass.ExceptionMessage(ex, this);
-            //}
+                Button button = sender as Button;
+                MainWindow.mainWindow.initializationMainTrack(button.Tag.ToString());
+            }
+            catch (Exception ex)
+            {
+                HelpClass.ExceptionMessage(ex, this);
+            }
         }
 
         private void Btn_fundAccountsReports_Click(object sender, RoutedEventArgs e)
---
+++ Restaurant/Restaurant/View/reports/accountsReports/uc_statementAccountsReports.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Restaurant.View.reports.accountsReports
+{
+    /// <summary>
+    /// Interaction logic for uc_statementAccountsReports.xaml
+    /// </summary>
+    public partial class uc_statementAccountsReports : UserControl
+    {
+        private static uc_statementAccountsReports _instance;
+        public static uc_statementAccountsReports Instance
+        {
+            get
+            {
+                _instance = new uc_statementAccountsReports();
+                return _instance;
+            }
+            set
+            {
+                _instance = value;
+            }
+        }
+        public uc_statementAccountsReports()
+        {
+            try
+            {
+                InitializeComponent();
+            }
+            catch
+            { }
+        }
+        private void UserControl_Unloaded(object sender, RoutedEventArgs e)
+        {
+            Instance = null;
+            GC.Collect();
+        }
+
+        private  void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
