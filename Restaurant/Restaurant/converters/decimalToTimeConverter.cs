533abdf990a6b5d42a267e93023ac43cb6493117
git-eng-dina <71547096+git-eng-dina@users.noreply.github.com>
2022-04-04 01:15:30 +0200

    remaining time converter

---
+++ Restaurant/Restaurant/converters/decimalToTimeConverter.cs
@@ -0,0 +1,29 @@
+﻿using Restaurant.Classes;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Restaurant.converters
+{
+    class decimalToTimeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            TimeSpan span = TimeSpan.FromMinutes(double.Parse( value.ToString()));
+ 
+
+            string label = (int)span.TotalMinutes + ":" + span.Seconds;
+            return label;
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
