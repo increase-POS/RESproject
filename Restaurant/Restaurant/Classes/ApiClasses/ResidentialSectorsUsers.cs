1cc41aafd273fc88de514b85a6e92b51e4484290
ahmadms2020 <najyms@gmail.com>
2022-04-07 15:10:06 +0300

    resdnt-user

--- Restaurant/Restaurant/Classes/ApiClasses/ResidentialSectorsUsers.cs
+++ Restaurant/Restaurant/Classes/ApiClasses/ResidentialSectorsUsers.cs
@@ -14,87 +14,77 @@ using System.Web;
 
 namespace Restaurant.Classes
 {
-    public class ResidentialSectors
+    public class ResidentialSectorsUsers
     {
-        public int residentSecId { get; set; }
-        public string name { get; set; }
+        public int id { get; set; }
+        public Nullable<int> residentSecId { get; set; }
+        public Nullable<int> userId { get; set; }
         public string notes { get; set; }
-        public byte isActive { get; set; }
         public Nullable<System.DateTime> createDate { get; set; }
         public Nullable<System.DateTime> updateDate { get; set; }
         public Nullable<int> createUserId { get; set; }
         public Nullable<int> updateUserId { get; set; }
 
-        public Boolean canDelete { get; set; }
 
-      
-
-        public async Task<List<ResidentialSectors>> Get()
+        public async Task<List<ResidentialSectorsUsers>> GetAll()
         {
-            List<ResidentialSectors> items = new List<ResidentialSectors>();
-            IEnumerable<Claim> claims = await APIResult.getList("ResidentialSectors/get");
+            List<ResidentialSectorsUsers> items = new List<ResidentialSectorsUsers>();
+            IEnumerable<Claim> claims = await APIResult.getList("ResidentialSectorsUsers/GetAll");
             foreach (Claim c in claims)
             {
                 if (c.Type == "scopes")
                 {
-                    items.Add(JsonConvert.DeserializeObject<ResidentialSectors>(c.Value, new IsoDateTimeConverter { DateTimeFormat = "dd/MM/yyyy" }));
+                    items.Add(JsonConvert.DeserializeObject<ResidentialSectorsUsers>(c.Value, new IsoDateTimeConverter { DateTimeFormat = "dd/MM/yyyy" }));
                 }
             }
             return items;
         }
-        public async Task<ResidentialSectors> GeById(int itemId)
+
+        public async Task<ResidentialSectorsUsers> GetById(int itemId)
         {
-            ResidentialSectors item = new ResidentialSectors();
+            ResidentialSectorsUsers item = new ResidentialSectorsUsers();
             Dictionary<string, string> parameters = new Dictionary<string, string>();
             parameters.Add("itemId", itemId.ToString());
             //#################
-            IEnumerable<Claim> claims = await APIResult.getList("ResidentialSectors/GeById", parameters);
+            IEnumerable<Claim> claims = await APIResult.getList("ResidentialSectorsUsers/GetById", parameters);
 
             foreach (Claim c in claims)
             {
                 if (c.Type == "scopes")
                 {
-                    item = JsonConvert.DeserializeObject<ResidentialSectors>(c.Value, new IsoDateTimeConverter { DateTimeFormat = "dd/MM/yyyy" });
+                    item = JsonConvert.DeserializeObject<ResidentialSectorsUsers>(c.Value, new IsoDateTimeConverter { DateTimeFormat = "dd/MM/yyyy" });
                     break;
                 }
             }
             return item;
         }
-        public async Task<int> Save(ResidentialSectors item)
+        public async Task<int> save(ResidentialSectorsUsers item)
         {
             Dictionary<string, string> parameters = new Dictionary<string, string>();
-            string method = "ResidentialSectors/Save";
+            string method = "ResidentialSectorsUsers/Save";
             var myContent = JsonConvert.SerializeObject(item);
             parameters.Add("itemObject", myContent);
            return await APIResult.post(method, parameters);
         }
-        public async Task<int> Delete(int itemId, int userId, Boolean final)
+        public async Task<int> delete(int itemId, int userId, Boolean final)
         {
             Dictionary<string, string> parameters = new Dictionary<string, string>();
             parameters.Add("itemId", itemId.ToString());
             parameters.Add("userId", userId.ToString());
             parameters.Add("final", final.ToString());
-            string method = "ResidentialSectors/Delete";
+            string method = "ResidentialSectorsUsers/Delete";
            return await APIResult.post(method, parameters);
         }
 
-        public async Task<List<ResidentialSectors>> GetResSectorsByUserId(int membershipId)
+        public async Task<int> UpdateResSectorsByUserId(List<ResidentialSectorsUsers> newList, int userId, int updateUserId)
         {
-            List<ResidentialSectors> items = new List<ResidentialSectors>();
             Dictionary<string, string> parameters = new Dictionary<string, string>();
-            parameters.Add("itemId", membershipId.ToString());
-            //#################
-            IEnumerable<Claim> claims = await APIResult.getList("ResidentialSectors/GetResSectorsByUserId", parameters);
-
-            foreach (Claim c in claims)
-            {
-                if (c.Type == "scopes")
-                {
-                    items.Add(JsonConvert.DeserializeObject<ResidentialSectors>(c.Value, new IsoDateTimeConverter { DateTimeFormat = "dd/MM/yyyy" }));
-
-                }
-            }
-            return items;
+            string method = "ResidentialSectorsUsers/UpdateResSectorsByUserId";
+            var newListParameter = JsonConvert.SerializeObject(newList);
+            parameters.Add("newList", newListParameter);
+            parameters.Add("userId", userId.ToString());
+            parameters.Add("updateUserId", updateUserId.ToString());
+            return await APIResult.post(method, parameters);
         }
     }
 }
