ba4e83847e750118fccc8265aab117cca6a1aa3d
yasmineNjr <82780508+yasmineNjr@users.noreply.github.com>
2022-04-20 13:30:24 +0300

    decimal to time converter

--- Restaurant/Restaurant/converters/decimalToTimeConverter.cs
+++ Restaurant/Restaurant/converters/decimalToTimeConverter.cs
@@ -13,13 +13,17 @@ namespace Restaurant.converters
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            TimeSpan span = TimeSpan.FromMinutes(double.Parse( value.ToString()));
-            var timeArr = span.ToString().Split(':');
+            if (value != null)
+            {
+                TimeSpan span = TimeSpan.FromMinutes(double.Parse(value.ToString()));
+                var timeArr = span.ToString().Split(':');
 
-            //string label = (int)span.TotalMinutes + ":" + span.Seconds;
-            string label = timeArr[1].ToString().PadLeft(2, '0') + ":" + Math.Round(decimal.Parse(timeArr[2])).ToString().PadLeft(2, '0'); 
-            return label;
-
+                //string label = (int)span.TotalMinutes + ":" + span.Seconds;
+                string label = timeArr[1].ToString().PadLeft(2, '0') + ":" + Math.Round(decimal.Parse(timeArr[2])).ToString().PadLeft(2, '0');
+                return label;
+            }
+            else
+                return "";
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
