d2d198ecaf2a042e00c827602741b9fe93c777f9
git-eng-dina <dinahnemah@gmail.com>
2022-05-31 14:38:40 +0200

    setup windows

--- Restaurant/Restaurant/Properties/Settings.Designer.cs
+++ Restaurant/Restaurant/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace Restaurant.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.1.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -56,6 +56,30 @@ namespace Restaurant.Properties {
             }
             set {
                 this["Lang"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        public string posId {
+            get {
+                return ((string)(this["posId"]));
+            }
+            set {
+                this["posId"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        public string APIUri {
+            get {
+                return ((string)(this["APIUri"]));
+            }
+            set {
+                this["APIUri"] = value;
             }
         }
     }
