3d5ebe329ac143211a1703136d52ce4fb6bd030b
yasmineNjr <82780508+yasmineNjr@users.noreply.github.com>
2022-07-05 13:04:10 +0300

    basicConverter

---
+++ Restaurant/Restaurant/converters/basicConverter.cs
@@ -0,0 +1,26 @@
+﻿using Restaurant.Classes;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Restaurant.converters
+{
+    class basicConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool s = (bool)value;
+            if (s) return AppSettings.resourcemanager.GetString("trBasic");
+            else return AppSettings.resourcemanager.GetString("trUnBasic");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
